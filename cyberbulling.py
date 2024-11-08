82823dbed25007612b0f8e5158499eb63e5c22db
Sandhanapandiyan <76581947+sandhanapandiyan@users.noreply.github.com>
2024-11-09 02:27:12 +0530

    Cyberbulling on social media

---
+++ cyberbulling.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from PIL import Image
+
+hide_menu = """
+<style>
+#MainMenu{
+    visibility:hidden;
+}
+
+footer{
+    visibility:hidden;
+}
+</style>
+"""
+
+showWarningOnDirectExecution = False
+image = Image.open('icons/logo.jpg')
+
+
+st.set_page_config(page_title = "CBDA", page_icon = image)
+
+st.markdown(hide_menu, unsafe_allow_html=True)
+
+st.sidebar.markdown("<br>", unsafe_allow_html=True)
+st.sidebar.image(image , use_column_width=True, output_format='auto')
+
+
+st.sidebar.markdown("---")
+
+
+st.sidebar.markdown("<br> <br> <br> <br> <br> <br> <h1 style='text-align: center; font-size: 18px; color: #0080FF;'> © 2023 | Ioannis Bakomichalis </h1>", unsafe_allow_html=True)
+
+st.image(image , use_column_width=True)
